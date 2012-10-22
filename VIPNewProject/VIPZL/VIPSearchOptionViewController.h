4cf0d401eea8bce2080ffd9a96690fd340e71984
liyuchao <371715647@qq.com>
2012-10-22 21:07:02 +0800

    职位搜索

---
+++ VIPNewProject/VIPZL/VIPSearchOptionViewController.h
@@ -0,0 +1,13 @@
+//
+//  VIPSearchOptionViewController.h
+//  VIPZL
+//
+//  Created by Ibokan on 12-10-22.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VIPSearchOptionViewController : UITableViewController
+
+@end
